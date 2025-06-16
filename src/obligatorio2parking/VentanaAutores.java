970e196eab9824e8362a92f8c8e150972a06485f
Lorenzo Aldao <aldaolorenzo@gmail.com>
2025-06-16 07:14:05 -0300

    VentanaAutores con Fotos

--- src/obligatorio2parking/VentanaAutores.java
+++ src/obligatorio2parking/VentanaAutores.java
@@ -4,17 +4,102 @@
  */
 package obligatorio2parking;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 /**
  *
  * @author MSI
  */
 public class VentanaAutores extends javax.swing.JFrame {
 
-    /**
-     * Creates new form VentanaEntradas
-     */
     public VentanaAutores() {
         initComponents();
+        cargarImagenes();
+        configurarVentana();
+    }
+    
+    private void cargarImagenes() {
+        // Cargar imagen para Lorenzo Aldao
+        cargarImagenAutor(jLabel1, "imagenes/lorenzo.png", "Lorenzo Aldao");
+        
+        // Cargar imagen para Jhonatan Adalid
+        cargarImagenAutor(jLabel7, "imagenes/jhonatan.png", "Jhonatan Adalid");
+    }
+    
+    private void cargarImagenAutor(JLabel label, String rutaImagen, String nombreAutor) {
+        try {
+            // Intentar cargar la imagen desde el archivo
+            File archivoImagen = new File(rutaImagen);
+            if (archivoImagen.exists()) {
+                BufferedImage imagenOriginal = ImageIO.read(archivoImagen);
+                Image imagenRedimensionada = imagenOriginal.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+                ImageIcon icono = new ImageIcon(imagenRedimensionada);
+                label.setIcon(icono);
+            } else {
+                // Si no existe la imagen, crear un placeholder
+                crearImagenPlaceholder(label, nombreAutor);
+            }
+        } catch (IOException e) {
+            // En caso de error, crear un placeholder
+            crearImagenPlaceholder(label, nombreAutor);
+        }
+        
+        // Configurar el label
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
+        label.setPreferredSize(new Dimension(100, 100));
+    }
+    
+    private void crearImagenPlaceholder(JLabel label, String nombreAutor) {
+        // Crear una imagen placeholder con las iniciales del autor
+        BufferedImage placeholder = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = placeholder.createGraphics();
+        
+        // Configurar antialiasing
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        // Fondo azul claro
+        g2d.setColor(new Color(173, 216, 230));
+        g2d.fillRect(0, 0, 100, 100);
+        
+        // Obtener iniciales
+        String iniciales = obtenerIniciales(nombreAutor);
+        
+        // Dibujar iniciales
+        g2d.setColor(Color.DARK_GRAY);
+        g2d.setFont(new Font("Arial", Font.BOLD, 24));
+        FontMetrics fm = g2d.getFontMetrics();
+        int x = (100 - fm.stringWidth(iniciales)) / 2;
+        int y = ((100 - fm.getHeight()) / 2) + fm.getAscent();
+        g2d.drawString(iniciales, x, y);
+        
+        g2d.dispose();
+        
+        ImageIcon icono = new ImageIcon(placeholder);
+        label.setIcon(icono);
+    }
+    
+    private String obtenerIniciales(String nombreCompleto) {
+        String[] partes = nombreCompleto.split(" ");
+        StringBuilder iniciales = new StringBuilder();
+        for (String parte : partes) {
+            if (!parte.isEmpty()) {
+                iniciales.append(parte.charAt(0));
+            }
+        }
+        return iniciales.toString().toUpperCase();
+    }
+    
+    private void configurarVentana() {
+        setTitle("Información de Autores");
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null); // Centrar la ventana
     }
 
     /**
@@ -33,6 +118,8 @@ public class VentanaAutores extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -65,35 +152,46 @@ public class VentanaAutores extends javax.swing.JFrame {
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(16, 16, 16)
+                .addGap(53, 53, 53)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel4)
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel2))
-                        .addGap(92, 92, 92)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addGap(40, 40, 40)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel6)
-                            .addComponent(jLabel5))))
-                .addContainerGap(101, Short.MAX_VALUE))
+                            .addComponent(jLabel5))
+                        .addContainerGap(116, Short.MAX_VALUE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel7)
+                        .addGap(151, 151, 151))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(20, 20, 20)
+                .addGap(22, 22, 22)
                 .addComponent(jLabel4)
-                .addGap(94, 94, 94)
+                .addGap(44, 44, 44)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel7))
+                .addGap(20, 20, 20)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel3))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel6)))
-                .addContainerGap(125, Short.MAX_VALUE))
+                        .addComponent(jLabel6))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3)))
+                .addContainerGap(153, Short.MAX_VALUE))
         );
 
         getContentPane().add(jPanel2);
@@ -108,9 +206,6 @@ public class VentanaAutores extends javax.swing.JFrame {
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -128,7 +223,6 @@ public class VentanaAutores extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(VentanaAutores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -139,11 +233,13 @@ public class VentanaAutores extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
